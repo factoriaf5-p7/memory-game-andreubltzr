cb635ade9c9913e2cefc848cdcef37127fa39c50
andreubltzr <andreubltzr11@gmail.com>
2023-08-28 10:07:02 +0200

    added tests for endpoints

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,26 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from './schema/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(name: string) {
+    const user = await this.userModel.findOne({ name });
+    if (!user)
+      throw new HttpException('Username already exist.', HttpStatus.NOT_FOUND);
+    return user;
   }
 }
